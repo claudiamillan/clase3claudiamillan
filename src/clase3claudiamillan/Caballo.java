1b102f785b9960538dd129d98e57b26a09338df5
claudiamillan <millanc569@gmail.com>
2017-10-26 19:41:54 -0300

    Creacion Clase con estructura completa atributos, constructor, y metodos

---
+++ src/clase3claudiamillan/Caballo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3claudiamillan;
+
+/**
+ *
+ * @author lab12
+ */
+public class Caballo 
+{
+    //Declaracion de atributos de Clase
+    String altura;
+    String color;
+    String peso;
+    String raza;
+    int patas = 4, edad;
+    
+    //Metodo constructor, no tiene retorno
+    public Caballo (String nuevoNombre)
+ { 
+     raza = nuevoNombre;
+}
+     //Metodos get y set de la clase
+     public void relinchar()
+     {
+     }
+     //Metodo para obtener la raza del caballo
+     public String obtenerRaza()
+     {
+         //Setencia obligatoria de retorno
+         return edad;
+         
+     }
+}
